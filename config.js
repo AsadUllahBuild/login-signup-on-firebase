025c52e356856af59ab10c1ca9e5b6cafbc61120
AsadUllahBuild <asadbuild97@gmail.com>
2023-11-19 12:37:15 +0500

    login signup form

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAmx2398XR9ZzgFBb9qKkXRU1Vx0wzSYdA",
     authDomain: "login-signup-form-e7239.firebaseapp.com",
@@ -16,3 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
--- home.js
+++ home.js
@@ -1,26 +1,110 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { auth } from "./config.js";
+import { auth, db } from "./config.js";
+import { collection, addDoc, getDocs, Timestamp, orderBy, query, where } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
-onAuthStateChanged(auth, (user) => {
+const form = document.querySelector('#form');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const card = document.querySelector("#card");
+const username = document.querySelector('#username');
+const profileImage = document.querySelector('#profileImage');
+
+//user loin or logout function
+
+onAuthStateChanged(auth, async (user) => {
     if (user) {
         const uid = user.uid;
-        console.log(uid);
+        const q = query(collection(db, "users"), where('uid', '==', uid));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data());
+            username.innerHTML = doc.data().name
+            profileImage.src = doc.data().profileUrl
+
+
+        });
     } else {
         window.location = 'index.html'
     }
 });
 
+//logout function
 
-const logout = document.querySelector("#logout-btn");
-
-logout.addEventListener('click', () => {
+const signout = document.querySelector('.logout')
+signout.addEventListener('click', () => {
     signOut(auth).then(() => {
         console.log('logout successfully');
-        window.location ='index.html'
-
+        window.location = 'index.html'
     }).catch((error) => {
-        console.log(errorMessage);
-
+        console.log(error);
     });
 })
 
+
+//get data from firestore
+
+
+let arr = []
+
+function renderPost() {
+    card.innerHTML = ''
+    arr.map((item) => {
+        card.innerHTML += `
+    <div class="card">
+    <div class="card-body">
+        <p class="card-text"><span class="h4">Title:</span>${item.title}</p>
+        <p><span class="h4">Description:</span> ${item.description}</p>
+        <button type="button" id="delete" class="btn btn-danger">Delete</button>
+        <button type="button" id="update" class="btn btn-info">Edit</button>
+    </div>
+    
+</div>`
+    })
+
+    const del = document.querySelectorAll('#delete');
+    const update = document.querySelectorAll('#update');
+}
+
+
+
+
+async function getDataFormFirestore() {
+    arr.length = 0;
+    const q = query(collection(db, "posts"), orderBy('postDate', 'desc'));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+        arr.push({ ...doc.data(), docId: doc.id });
+    });
+    console.log(arr);
+    renderPost();
+}
+getDataFormFirestore()
+
+//post data on firestore
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    try {
+        const postObj ={
+            title: title.value,
+            description: description.value,
+            Uid: auth.currentUser.uid,
+            postDate: Timestamp.fromDate(new Date())
+        }
+        const docRef = await addDoc(collection(db, "posts"), postObj );
+            
+       
+        console.log("Document written with ID: ", docRef.id);
+        postObj.docId = doc.id;
+        arr=[postObj,...arr];
+        console.log(arr);
+        renderPost();
+
+
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+
+
+})
